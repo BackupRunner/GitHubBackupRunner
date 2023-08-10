87023def14e1af1eb8c02c842ec7b24a9f5fad94
Vineel Sai <mail@vineelsai.com>
2023-08-10 17:43:11 +0530

    fix upload

--- src/index.ts
+++ src/index.ts
@@ -89,57 +89,76 @@ try {
     })
 
     const date = new Date().getUTCDate() + "-" + (new Date().getUTCMonth() + 1) + "-" + new Date().getUTCFullYear()
+    const FILE_PATH = "repos.tar.gz"
+    const DROPBOX_UPLOAD_PATH = `/repos-${date.toLowerCase()}.tar.gz`
 
-    // Create upload session
-    dbx.filesUploadSessionStart({ close: false })
-        .then(response => {
-            const sessionId = response.result.session_id
-            const readStream = fs.createReadStream("repos.tar.gz")
-            let uploadedSize = 0
-            readStream.on('data', chunk => {
-                uploadedSize += chunk.length
+    // Open the file and get its size
+    fs.stat(FILE_PATH, (err, stats) => {
+        if (err) {
+            console.error('Error opening file:', err)
+            return
+        }
 
-                // Append data to upload session
-                dbx.filesUploadSessionAppendV2({
-                    contents: chunk,
-                    cursor: {
-                        session_id: sessionId,
-                        offset: uploadedSize - chunk.length
-                    },
-                    close: false
-                }).then(() => {
-                    if (uploadedSize >= fs.statSync("repos.tar.gz").size) {
-                        // Finalize upload session
-                        dbx.filesUploadSessionFinish({
-                            cursor: {
-                                session_id: sessionId,
-                                offset: uploadedSize
-                            },
-                            commit: {
-                                path: `/Backup/${date}/repos.tar.gz`,
-                                mode: { '.tag': 'add' },
-                                autorename: false,
-                                mute: false
-                            }
-                        }).then(response => {
-                            console.log('File uploaded successfully:', response)
-                        }).catch(error => {
-                            console.error('Error finishing upload session:', error)
-                        })
-                    }
-                }).catch(error => {
-                    console.error('Error appending to upload session:', error)
-                })
+        const fileSize = stats.size
+        const chunkSize = 100 * 1024 * 1024 // 5MB
+        const numChunks = Math.ceil(fileSize / chunkSize)
+
+        // Start a new upload session
+        dbx.filesUploadSessionStart({ close: false })
+            .then(response => {
+                const sessionId = response.result.session_id
+                let cursorPosition = 0
+                let chunkIndex = 0
+
+                function uploadNextChunk() {
+                    const chunkBuffer = Buffer.alloc(chunkSize)
+                    const bytesRead = fs.readSync(fs.openSync(FILE_PATH, 'r'), chunkBuffer, 0, chunkSize, cursorPosition)
+
+                    cursorPosition += bytesRead
+
+                    // Append data to the upload session
+                    dbx.filesUploadSessionAppendV2({
+                        contents: chunkBuffer.subarray(0, bytesRead),
+                        cursor: {
+                            session_id: sessionId,
+                            offset: chunkIndex * chunkSize
+                        }
+                    }).then(() => {
+                        chunkIndex++
+                        if (chunkIndex < numChunks) {
+                            uploadNextChunk()
+                        } else {
+                            // Finish the upload session
+                            dbx.filesUploadSessionFinish({
+                                cursor: {
+                                    session_id: sessionId,
+                                    offset: fileSize
+                                },
+                                commit: {
+                                    path: DROPBOX_UPLOAD_PATH,
+                                    autorename: true,
+                                    mute: false
+                                }
+                            }).then(_ => {
+                                console.log('File uploaded successfully:')
+                            }).catch(error => {
+                                console.error('Error finishing upload session:', error)
+                            })
+                        }
+                    }).catch(error => {
+                        console.error('Error appending to upload session:', error)
+                        process.exit(1)
+                    })
+                }
+
+                uploadNextChunk()
             })
-
-            readStream.on('end', () => {
-                console.log('File reading completed.')
+            .catch(error => {
+                console.error('Error starting upload session:', error)
+                process.exit(1)
             })
-
-        })
-        .catch(error => {
-            console.error('Error starting upload session:', error)
-        })
+    })
 } catch (err) {
     console.error(err)
+    process.exit(1)
 }
